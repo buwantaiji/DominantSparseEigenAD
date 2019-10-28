a70397ab65e98d8548733e583fccc880410b9294
Hao Xie <xiehao18@mails.ucas.edu.cn>
2019-10-28 17:33:45 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DominantSparseEigenAD",
+    version="0.0.1",
+    author="Hao Xie",
+    author_email="xiehao18@iphy.ac.cn",
+    description="Backward AD of dominant sparse eigensolver using Pytorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=" automatic differentiation dominant eigensolver Pytorch",
+    url="https://github.com/buwantaiji/DominantSparseEigenAD",
+    packages=["DominantSparseEigenAD"],
+    install_requires=[
+            "numpy",
+            "torch>=1.3.0",
+        ],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
